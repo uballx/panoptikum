39e1ad2f46679e3838a743d67ba632abfd4947ad
Konst Koral <mymail@gmail.com>
2022-06-27 14:44:56 +0200

    csrf

--- panoptikum/settings.py
+++ panoptikum/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS='https://immense-refuge-95808.herokuapp.com/'
+CSRF_TRUSTED_ORIGINS = ['https://immense-refuge-95808.herokuapp.com/']
 
 import django_heroku
 django_heroku.settings(locals())
