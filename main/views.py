58eb779ae34ba9bf5dcb196d4e17539abda350b4
Konst Koral <mymail@gmail.com>
2022-06-28 09:23:26 +0200

    minor

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
+from django.contrib import messages
+
+def homepage(request):
+    messages.add_message(request, messages.info, "User logged in")
+    return render(
+        request,
+        template_name="base.html",
+        context=()
+    )
 
 def register_request(request):
     if request.method == "POST":
@@ -8,6 +17,7 @@ def register_request(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            homepage(request)
             return redirect("/")
     else:
         form = NewUserForm()
